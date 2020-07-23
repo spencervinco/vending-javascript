62775ccaad6db2c8895edf6ff37f61785849dc56
Spencer Vinco <Spencer_Vinco@gap.com>
2020-07-23 10:59:10 -0400

    session 2

--- vending.js
+++ vending.js
@@ -4,12 +4,48 @@ class Vending {
         this.balance = 0.0;
         this.coinLookup = {
             "nickle": .05,
-            "dime": .1
-        }
+            "dime": .1,
+            "quarter": .25
+        };
+        this.productLookUp = {
+            "1": {
+                name: "cola",
+                price: 1.00
+            }
+        };
+        this.rejectedCoins = [];
+        this.displayText = '';
     }
-    getDisplay = () => this.balance === 0.0 ? "INSERT COINS" : ("balance: " + this.balance.toFixed(2));
+    getDisplay = () => {
+        let currentDisplay = this.displayText;
+        if (currentDisplay.length === 0) {
+            currentDisplay = this.balance === 0.0 ? "INSERT COINS" : ("balance: " + this.balance.toFixed(2));
+        }
+        this.displayText = '';
+        return currentDisplay;
+    }
     insertCoin = (coinName) => {
-        this.balance += this.coinLookup[coinName] ? this.coinLookup[coinName] : 0.0;
+        const identifiedCoin = this.coinLookup[coinName];
+
+        if (!identifiedCoin) {
+            this.rejectedCoins.push(coinName);
+        } else {
+            this.balance += identifiedCoin;
+        }
+
+    }
+    getChange = () => {
+        return this.rejectedCoins;
+    }
+    selectProduct = (productNumber) => {
+        let selectedProduct = this.productLookUp[parseInt(productNumber)];
+        if (this.balance < selectedProduct.price) {
+            this.displayText = "PRICE $" + selectedProduct.price.toFixed(2);
+        }
+        else {
+            this.balance = 0;
+            this.displayText = "THANK YOU";
+        }
     }
 }
 
--- vending.spec.js
+++ vending.spec.js
@@ -21,6 +21,12 @@ test('increases balance by .10 when a dime is inserted', () => {
     expect(vendingRef.getDisplay()).toBe("balance: 0.10");
 });
 
+test('increases balance by .25 when a quarter is inserted', () => {
+    let vendingRef = new Vending();
+    vendingRef.insertCoin("quarter");
+    expect(vendingRef.getDisplay()).toBe("balance: 0.25");
+});
+
 test('does not increases balance when a penny is inserted', () => {
     let vendingRef = new Vending();
     vendingRef.insertCoin("dime");
@@ -28,8 +34,63 @@ test('does not increases balance when a penny is inserted', () => {
     expect(vendingRef.getDisplay()).toBe("balance: 0.10");
 });
 
-test('does not increases balance when a penny is inserted', () => {
+test('balance should be equal to .35', () => {
+    let vendingRef = new Vending();
+    vendingRef.insertCoin("dime");
+    vendingRef.insertCoin("quarter");
+    expect(vendingRef.getDisplay()).toBe("balance: 0.35");
+});
+
+test('odd coins are returned from rejectedCoins function', () => {
     let vendingRef = new Vending();
     vendingRef.insertCoin("penny");
-    expect(vendingRef.rejectedCoins.length).toBeGreaterThan(0);
+    expect(vendingRef.getChange().length).toBeGreaterThan(0);
 });
+
+//Select Product
+describe("Select Product", () => {
+    test('should display THANK YOU after selecting a product', () => {
+        let vendingRef = new Vending();
+        vendingRef.insertCoin('quarter');
+        vendingRef.insertCoin('quarter');
+        vendingRef.insertCoin('quarter');
+        vendingRef.insertCoin('quarter');
+        vendingRef.selectProduct(1);
+        expect(vendingRef.getDisplay()).toBe('THANK YOU');
+    });
+
+    test('should display a INSERT COIN if get display is checked twice.', () => {
+        let vendingRef = new Vending();
+        vendingRef.insertCoin('quarter');
+        vendingRef.insertCoin('quarter');
+        vendingRef.insertCoin('quarter');
+        vendingRef.insertCoin('quarter');
+        vendingRef.selectProduct(1);
+        vendingRef.getDisplay(); //thank you
+        expect(vendingRef.getDisplay()).toBe('INSERT COINS');
+    });
+
+    test('should set the amount to 0.00 after displaying Thank you', () => {
+        let vendingRef = new Vending();
+        vendingRef.insertCoin('quarter');
+        vendingRef.insertCoin('quarter');
+        vendingRef.insertCoin('quarter');
+        vendingRef.insertCoin('quarter');
+        vendingRef.selectProduct(1);
+        vendingRef.getDisplay(); //thank you
+        expect(vendingRef.balance).toBe(0.00);
+    });
+
+    test('should display price of cola if balance is less than price', () => {
+        let vendingRef = new Vending();
+        vendingRef.selectProduct(1);
+        expect(vendingRef.getDisplay()).toBe('PRICE $1.00');
+    });
+
+    test('should display price of chips if balance is less than price', () => {
+        let vendingRef = new Vending();
+        vendingRef.selectProduct(2);
+        expect(vendingRef.getDisplay()).toBe('PRICE $0.50');
+    });
+});
+
